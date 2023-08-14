9ff299fd808dad63e6e412ec7d1191a8a8976289
0seoYun <yys020819@gmail.com>
2023-08-14 15:14:30 +0900

    update까지 구현

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import './styles.css';
 
 interface TodoProps {
     text: string;
     completed: boolean;
     onToggle: () => void;
     onDelete: () => void;
+    children: React.ReactNode; // Edit 버튼을 위해 children 추가
 }
 
-const Todo: React.FC<TodoProps> = ({ text, completed, onToggle, onDelete }) => {
+
+const Todo: React.FC<TodoProps> = ({ text, completed, onToggle, onDelete, children }) => {
     return (
-        <div>
+        <div className="todo">
             <input type="checkbox" checked={completed} onChange={onToggle} />
             <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{text}</span>
+            {children} {/* Edit 버튼 여기 */}
             <button onClick={onDelete}>Delete</button>
         </div>
     );
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -11,6 +11,8 @@ interface TodoItem {
 
 const TodoList: React.FC = () => {
     const [todos, setTodos] = useState<TodoItem[]>([]);
+    const [editingId, setEditingId] = useState<number | null>(null);
+    const [editingText, setEditingText] = useState<string>('');
 
     const addTodo = (text: string) => {
         const newTodo: TodoItem = { id: Date.now(), text, completed: false };
@@ -29,21 +31,64 @@ const TodoList: React.FC = () => {
         setTodos(todos.filter((todo) => todo.id !== id));
     };
 
+    const startEditing = (id: number, text: string) => {
+        setEditingId(id);
+        setEditingText(text);
+    };
+
+    const cancelEditing = () => {
+        setEditingId(null);
+        setEditingText('');
+    };
+
+    const saveEditing = (id: number) => {
+        if (editingText.trim() === '') {
+            return;
+        }
+
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, text: editingText } : todo
+            )
+        );
+
+        setEditingId(null);
+        setEditingText('');
+    };
+
 
     return (
         <div>
             <TodoInput onAddTodo={addTodo} />
             {todos.map((todo) => (
-                <Todo
-                    key={todo.id}
-                    text={todo.text}
-                    completed={todo.completed}
-                    onToggle={() => toggleTodo(todo.id)}
-                    onDelete={() => deleteTodo(todo.id)}
-                />
+                <div key={todo.id}>
+                    {editingId === todo.id ? (
+                        <>
+                            <input
+                                type="text"
+                                value={editingText}
+                                onChange={(e) => setEditingText(e.target.value)}
+                            />
+                            <button onClick={() => saveEditing(todo.id)}>Save</button>
+                            <button onClick={cancelEditing}>Cancel</button>
+                        </>
+                    ) : (
+                        <Todo
+                            text={todo.text}
+                            completed={todo.completed}
+                            onToggle={() => toggleTodo(todo.id)}
+                            onDelete={() => deleteTodo(todo.id)}
+                        >
+                            <button onClick={() => startEditing(todo.id, todo.text)}>
+                                Edit
+                            </button>
+                        </Todo>
+                    )}
+                </div>
             ))}
         </div>
     );
 };
+
 
 export default TodoList;
